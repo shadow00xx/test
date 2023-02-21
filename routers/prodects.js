f47451a477cb7847bb0937ec07374b94156f99e1
shadow00xx <117114498+shadow00xx@users.noreply.github.com>
2023-02-21 20:45:05 +0200

    Add files via upload

--- routers/prodects.js
+++ routers/prodects.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const { ensureAuth } = require('../middlewares/auth')
-const upload = require('../middlewares/upload');
+const upload = require('../middlewares/up');
 
 const prodectsContrller = require('../controllers/prodectsContrller')
 
