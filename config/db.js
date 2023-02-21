1449755ef44c1cc4a6d2fa4f673482d9b3fe42a4
shadow00xx <117114498+shadow00xx@users.noreply.github.com>
2023-02-21 19:09:27 +0200

    Add files via upload

--- config/db.js
+++ config/db.js
@@ -1,9 +1,11 @@
+
 const mongoose = require('mongoose');
 
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MANGO_URI)
+        mongoose.set("strictQuery", false)
+        const conn = await mongoose.connect(process.env.MANGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
         console.log(`mongoDB connected to ${conn.connection.host}`);
     } catch (err) {
         console.log(err)
