cd5c013544309bac67df3828cb65ee719f03598c
shadow00xx <117114498+shadow00xx@users.noreply.github.com>
2026-09-11 22:55:22 +0200

    security: harden user registration and profile authorization

--- controllers/userContrller.js
+++ controllers/userContrller.js
@@ -1,179 +1,126 @@
-const path = require('path');
-const User = require('../models/User')
+const User = require('../models/User');
 const passport = require('passport');
 const bcrypt = require('bcryptjs');
-const prodects = require('../models/Prodects')
+const prodects = require('../models/Prodects');
 
-
-
-
-let errors = []
-
-// get
-// login
+// Login
 exports.login = (req, res) => {
-    res.render('pages/login', { title: ' تسجيل الدخول',layout:"layouts/register" })
-}
+    res.render('pages/login', { title: ' تسجيل الدخول', layout: 'layouts/register' });
+};
 
-// get
-// signup
+// Signup
 exports.signup = (req, res) => {
-    res.render('pages/signup', { title: ' التسجيل',layout:"layouts/register"  })
-}
+    res.render('pages/signup', { title: ' التسجيل', layout: 'layouts/register' });
+};
 
-// post 
-// signup
 exports.signupPost = async (req, res) => {
-
-
     try {
-        const { displayName, username, password } = req.body
+        const displayName = String(req.body.displayName || '').trim();
+        const username = String(req.body.username || '').trim();
+        const password = String(req.body.password || '');
 
-        const user = await User.findOne({ username: username })
-        if (user) {
-            errors.push({ msg: 'المستخدم موجود بالفعل' })
-
-            res.render('pages/signup', {
-                errors
-            })
-        } else {
-            const newUser = await new User({ displayName, username, password, })
-            bcrypt.genSalt(10, (err, salt) => {
-                bcrypt.hash(newUser.password, salt, async (err, hash) => {
-                    if (err) throw err;
-                    newUser.password = hash
-                    console.log(newUser)
-                    await newUser.save()
-                })
-            })
-            req.flash(
-                'success_msg',
-                'تم التسجيل بنجاح'
-            );
-            res.redirect('/user/login')
+        if (!displayName || !username || password.length < 6) {
+            req.flash('error_msg', 'الرجاء إدخال البيانات بشكل صحيح وكلمة مرور لا تقل عن 6 أحرف');
+            return res.redirect('/user/signup');
         }
-    } catch (err) {
-        console.log(err)
-    }
-}
 
-// post
-// login
+        const existingUser = await User.findOne({ username });
+        if (existingUser) {
+            req.flash('error_msg', 'المستخدم موجود بالفعل');
+            return res.redirect('/user/signup');
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 12);
+        await User.create({ displayName, username, password: hashedPassword });
+
+        req.flash('success_msg', 'تم التسجيل بنجاح');
+        return res.redirect('/user/login');
+    } catch (err) {
+        console.error(err);
+        return res.render('error/500');
+    }
+};
+
+// Login
 exports.loginPost = (req, res, next) => {
     passport.authenticate('local', {
         successRedirect: '/',
         failureRedirect: '/user/login',
-        failureFlash: true
+        failureFlash: true,
     })(req, res, next);
-}
+};
 
-// get
-// profile
-exports.profile = async (req, res) => {
-    
+// Current user's profile
+exports.profile = (req, res) => {
+    return res.render('profile', { user: req.user, title: 'الملف الشخصي' });
+};
 
-        // const user = await User.findOne( { _id: req.params._id })
-        // .lean()
-        // const userpro = await prodects.find({user:req.params._id })
-        //      .populate('user')
-        //      .lean()
-
-        res.render('profile', { user:req.user, title: 'الملف الشخصي' })
-
-    
-
-}
-
-const mongoose = require("mongoose");
-const fs = require('fs');
-
-const cloudinary = require("../utils/cloudinary");
-
-// @desc    Show edit page
-// @route   GET /profile/edit/:id
+// Edit only the current user's safe profile fields.
 exports.editProfile = async (req, res) => {
     try {
-        // const result = await cloudinary.uploader.upload(req.file.path)
+        const allowedFields = ['displayName', 'firstName', 'lastName', 'image'];
+        const updates = {};
 
-        let user = await User.find({ _id: req.user.id }).lean()
+        for (const field of allowedFields) {
+            if (req.body[field] !== undefined) {
+                updates[field] = String(req.body[field]).trim();
+            }
+        }
 
-const v = req.body
-// if (req.file) {
-//     // prodect.image = req.file.fileName
-//     user.image = result.secure_url
-// }
+        // Do not allow profile editing to change identity, password, or privileges.
+        delete updates.username;
+        delete updates.password;
+        delete updates.isAdmin;
+        delete updates.isStore;
+        delete updates.isVerified;
+        delete updates.isowner;
+        delete updates.provider;
+        delete updates.googleId;
+        delete updates.facebookId;
 
-// if (user.cloudinary_id) {
-//     cloudinary_id: result.public_id
-// }
-
-
-        user = await User.findOneAndUpdate({ _id: req.user.id }, v,
-            {
+        const updatedUser = await User.findByIdAndUpdate(req.user._id, updates, {
             new: true,
             runValidators: true,
-        })
+        });
 
-        res.redirect('/')
-    }
-    catch (err) {
-        console.error(err)
-        return res.render('error/500')
-    }
-}
+        if (!updatedUser) {
+            return res.status(404).render('error/404');
+        }
 
-//   get
-// logout
-exports.logout = (req, res) => {
-    req.logout(req.user, err => {
-        if (err) return next(err);
-        res.redirect("/user/login");
-    });
-}
-
-// showUsersMyProfile
-exports.showUsersMyProfile = async (req, res) => {
-    
-try {
-    const user = await User.findOne( { _id: req.params.id })
-    .lean()
-    const userpro = await prodects.find({user:req.params.id }) 
-         .populate('user')
-         .lean()
-
-    res.render('pages/showUsersMyProfile', { user,userpro, title: 'الملف الشخصي' })
-} catch (error) {
-    console.error(err)
-    return res.render('error/500')
-}
-  
-
-
-
-}
-
-// editpro
-exports.editPro = async (req, res) => {
-    try {
-      let checkUser = await User.findById({id:req.user.id})
-  
-  
-      if (!checkUser) {
-        return res.send('error/404')
-      }
-  
-      if (checkUser != req.user.id) {
-        res.redirect('/')
-      } else {
-        checkUser = await User.findOneAndUpdate({ id: req.params.id }, req.body, {
-          new: true,
-          runValidators: true,
-        })
-  
-        res.redirect('/')
-      }
+        return res.redirect('/user/profile');
     } catch (err) {
-      console.error(err)
-      return res.render('error/500')
+        console.error(err);
+        return res.render('error/500');
     }
-  }
+};
+
+// Logout
+exports.logout = (req, res, next) => {
+    req.logout((err) => {
+        if (err) return next(err);
+        req.session?.destroy(() => res.redirect('/user/login'));
+    });
+};
+
+// Public profile of another user.
+exports.showUsersMyProfile = async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id).lean();
+        if (!user) {
+            return res.status(404).render('error/404');
+        }
+
+        const userpro = await prodects.find({ user: req.params.id })
+            .populate('user')
+            .lean();
+
+        return res.render('pages/showUsersMyProfile', {
+            user,
+            userpro,
+            title: 'الملف الشخصي',
+        });
+    } catch (err) {
+        console.error(err);
+        return res.render('error/500');
+    }
+};
