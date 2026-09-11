963e9c23cd9455b57dee923bae4534ba7e2632b7
shadow00xx <117114498+shadow00xx@users.noreply.github.com>
2026-09-11 22:55:49 +0200

    security: strengthen user schema validation

--- models/User.js
+++ models/User.js
@@ -1,48 +1,51 @@
-const mongoose = require('mongoose')
-
+const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-
     username: {
         type: String,
-        require: true
+        trim: true,
+        minlength: 3,
+        maxlength: 80,
     },
     password: {
         type: String,
-        require: true
     },
-   facebookId: {
+    googleId: {
         type: String,
-        // required: true,
-    },   
-      provider: {
+    },
+    facebookId: {
         type: String,
-      },
+    },
+    provider: {
+        type: String,
+        enum: ['local', 'google', 'facebook'],
+    },
     displayName: {
         type: String,
         required: true,
+        trim: true,
+        maxlength: 120,
     },
     firstName: {
         type: String,
-        // required: true,
+        trim: true,
+        maxlength: 80,
     },
     lastName: {
         type: String,
-        // required: true,
+        trim: true,
+        maxlength: 80,
     },
     image: {
         type: String,
     },
     cloudinary_id: {
         type: String,
-      },
+    },
     isVerified: { type: Boolean, default: false },
-    isStore:{type: Boolean, default: false },
-    isAdmin:{type: Boolean, default: false },
-    isowner:{type: Boolean, default: false },
+    isStore: { type: Boolean, default: false },
+    isAdmin: { type: Boolean, default: false },
+    isowner: { type: Boolean, default: false },
+}, { timestamps: true });
 
-},
-    { timestamps: true }
-)
-
-module.exports = mongoose.model('User', UserSchema)
+module.exports = mongoose.model('User', UserSchema);
