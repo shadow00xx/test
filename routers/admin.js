feaf440b66012598dc183adc1277da4817de8010
shadow00xx <117114498+shadow00xx@users.noreply.github.com>
2026-09-11 22:52:27 +0200

    security: protect admin routes with current-user authorization

--- routers/admin.js
+++ routers/admin.js
@@ -1,20 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const adminContrller = require('../controllers/adminContrller')
-const {admin, } = require('../middlewares/userType')
+const adminContrller = require('../controllers/adminContrller');
+const { ensureAuth } = require('../middlewares/auth');
+const { admin } = require('../middlewares/userType');
 
+// Admin-only routes
+router.get('/showreport', ensureAuth, admin, adminContrller.showreport);
+router.get('/showcontacts', ensureAuth, admin, adminContrller.showcontact);
 
-
-
-
-
-// showreport
-router.get('/showreport', adminContrller.showreport);
-
-
-// showcontacts
-router.get('/showcontacts', adminContrller.showcontact);
-
-
-
-module.exports = router
+module.exports = router;
