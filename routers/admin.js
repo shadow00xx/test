1baff28e1ffe1b86d38209742372428ab1712e64
shadow00xx <117114498+shadow00xx@users.noreply.github.com>
2026-09-11 23:01:05 +0200

    Add admin report dismissal route

--- routers/admin.js
+++ routers/admin.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const adminContrller = require('../controllers/adminContrller');
 const { ensureAuth } = require('../middlewares/auth');
 const { admin } = require('../middlewares/userType');
+const checkObjectId = require('../middlewares/checkObjectId');
 
 // Admin-only routes
 router.get('/showreport', ensureAuth, admin, adminContrller.showreport);
+router.put('/reports/:id/dismiss', ensureAuth, admin, checkObjectId('id'), adminContrller.dismissReport);
 router.get('/showcontacts', ensureAuth, admin, adminContrller.showcontact);
 
 module.exports = router;
