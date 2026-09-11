00f28995213a26974281ea95f3fa28552563c2d2
shadow00xx <117114498+shadow00xx@users.noreply.github.com>
2026-09-11 22:55:33 +0200

    security: improve Google authentication error handling

--- config/passportGoogle.js
+++ config/passportGoogle.js
@@ -1,6 +1,5 @@
-const GoogleStrategy = require('passport-google-oauth20').Strategy
-const mongoose = require('mongoose')
-const User = require('../models/User')
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const User = require('../models/User');
 
 module.exports = function (passport) {
     passport.use(
@@ -11,35 +10,27 @@ module.exports = function (passport) {
                 callbackURL: '/auth/google/callback',
             },
             async (accessToken, refreshToken, profile, done) => {
-                const newUser = {
-                    googleId: profile.id,
-                    displayName: profile.displayName,
-                    firstName: profile.name.givenName,
-                    lastName: profile.name.familyName,
-                    image: profile.photos[0].value,
-                }
-
                 try {
-                    let user = await User.findOne({ googleId: profile.id })
+                    const image = profile.photos?.[0]?.value;
+                    const newUser = {
+                        googleId: profile.id,
+                        displayName: profile.displayName || 'Google User',
+                        firstName: profile.name?.givenName,
+                        lastName: profile.name?.familyName,
+                        image,
+                        provider: 'google',
+                    };
 
-                    if (user) {
-                        done(null, user)
-                    } else {
-                        user = await User.create(newUser)
-                        done(null, user)
+                    let user = await User.findOne({ googleId: profile.id });
+                    if (!user) {
+                        user = await User.create(newUser);
                     }
+
+                    return done(null, user);
                 } catch (err) {
-                    console.error(err)
+                    return done(err);
                 }
             }
         )
-    )
-
-    passport.serializeUser((user, done) => {
-        done(null, user.id)
-    })
-
-    passport.deserializeUser((id, done) => {
-        User.findById(id, (err, user) => done(err, user))
-    })
-}
+    );
+};
