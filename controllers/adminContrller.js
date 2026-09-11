220c9dd213d86ff2504386d1c4490f3032aa3aa1
shadow00xx <117114498+shadow00xx@users.noreply.github.com>
2026-09-11 22:53:19 +0200

    fix: make reports compatible with the new user-linked report system

--- controllers/adminContrller.js
+++ controllers/adminContrller.js
@@ -1,40 +1,28 @@
-const User = require('../models/User')
-const prodects = require('../models/Prodects')
-
-const contacts = require('../models/Contact')
-
-
-
-
+const prodects = require('../models/Prodects');
+const contacts = require('../models/Contact');
 
 exports.showreport = async (req, res) => {
     try {
+        const pro = await prodects.find({
+            $or: [
+                { 'reports.0': { $exists: true } },
+                { report: 444 },
+            ],
+        }).sort({ _id: -1 });
 
-        const pro = await prodects.find({report:444}).sort({ _id: -1 });
-
-        res.render('pages/showreport', { pro })
-
+        res.render('pages/showreport', { pro });
     } catch (err) {
-        console.log(err)
-        res.render('error/500')
+        console.error(err);
+        res.render('error/500');
     }
-}
+};
 
-
-
-
-
-
-// get
-// show showcontact 
-exports.showcontact = async(req, res)=>{
+exports.showcontact = async (req, res) => {
     try {
-const contact = await contacts.find().sort('desc')
-     .populate('createby')
-
-        res.render('pages/showcontacts', {contact})
+        const contact = await contacts.find().sort({ createdAt: -1 }).populate('createby');
+        res.render('pages/showcontacts', { contact });
     } catch (err) {
-        console.log(err);
-        res.render('error/500')
+        console.error(err);
+        res.render('error/500');
     }
-}
+};
